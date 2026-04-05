797a6e706933fb7b61e96c73744f54edf590bd97
Melodieshusband <totorkulovredzeb123@gmail.com>
2026-04-05 14:38:50 +0300

    Add files via upload

---
+++ deobfuscated/Test_deobfuscated.lua
@@ -0,0 +1,122 @@
+--[[ MoonSec V3 Deobfuscated ]]
+--[[ Key: 53 ]]
+
+--[[ Roblox API Calls Detected ]]
+-- Enum
+-- Players
+-- game
+-- loadstring
+-- pairs
+
+--[[ String Constants ]]
+-- "Cilent Right Korblox"
+-- "DarkTheme"
+-- "KeyCode"
+-- "This file was protected with MoonSec V3 by Federal#9999"
+-- "NewTab"
+-- "NewKeybind"
+-- "CreateLib"
+-- "Gives Fake Korblox"
+-- "_TIfvHPaIwCyW"
+-- "Main"
+-- "Toggle GUI"
+-- "NewSection"
+-- "Gives Fake Headless"
+-- "Keybind"
+-- "Main Stuffs"
+-- "Changes Keybind"
+-- "HttpGet"
+-- "Cilent Left Korblox"
+-- "NewButton"
+-- "Headless And Korblox"
+-- "Cilent Headless"
+-- "https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"
+-- "MoonSec_StringsHiddenAttr6"
+-- "ToggleUI"
+-- "Character"
+-- "http://www.roblox.com/asset/?id=902942096"
+-- "Transparency"
+-- "RightLowerLeg"
+-- "http://www.roblox.com/asset/?id=902942089"
+-- "http://roblox.com/asset/?id=902843398"
+-- "MeshId"
+-- "RightUpperLeg"
+-- "TextureID"
+-- "RightFoot"
+-- "http://www.roblox.com/asset/?id=902942093"
+-- "LocalPlayer"
+-- "GetService"
+-- "LeftLowerLeg"
+-- "http://www.roblox.com/asset/?id=902942101"
+-- "LeftFoot"
+-- "http://roblox.com/asset/?id=902842271"
+-- "http://www.roblox.com/asset/?id=902942077"
+-- "LeftUpperLeg"
+-- "http://www.roblox.com/asset/?id=902942082"
+-- "Head"
+-- "GetChildren"
+-- "Decal"
+-- "IsA"
+-- "Players "
+
+--[[ Raw Constant Table ]]
+local _constants = {
+    "Cilent Right Korblox",
+    "DarkTheme",
+    "KeyCode",
+    "This file was protected with MoonSec V3 by Federal#9999",
+    "NewTab",
+    "NewKeybind",
+    "CreateLib",
+    "Gives Fake Korblox",
+    "_TIfvHPaIwCyW",
+    "Main",
+    "Toggle GUI",
+    "NewSection",
+    "Gives Fake Headless",
+    "Keybind",
+    "Main Stuffs",
+    "Changes Keybind",
+    "HttpGet",
+    "Cilent Left Korblox",
+    "NewButton",
+    "Headless And Korblox",
+    "Cilent Headless",
+    "https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua",
+    "MoonSec_StringsHiddenAttr6",
+    "ToggleUI",
+    "Character",
+    "http://www.roblox.com/asset/?id=902942096",
+    "Transparency",
+    "RightLowerLeg",
+    "http://www.roblox.com/asset/?id=902942089",
+    "http://roblox.com/asset/?id=902843398",
+    "MeshId",
+    "RightUpperLeg",
+    "TextureID",
+    "RightFoot",
+    "http://www.roblox.com/asset/?id=902942093",
+    "LocalPlayer",
+    "GetService",
+    "LeftLowerLeg",
+    "http://www.roblox.com/asset/?id=902942101",
+    "LeftFoot",
+    "http://roblox.com/asset/?id=902842271",
+    "http://www.roblox.com/asset/?id=902942077",
+    "LeftUpperLeg",
+    "http://www.roblox.com/asset/?id=902942082",
+    "Head",
+    "GetChildren",
+    "Decal",
+    "IsA",
+    "Players ",
+}
+
+--[[ Roblox API Table ]]
+local _api = {
+    "Enum",
+    "Players",
+    "game",
+    "loadstring",
+    "pairs",
+}
