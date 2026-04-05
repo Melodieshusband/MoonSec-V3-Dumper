f10fe1826de4c3d8d93a4fc6dd7bea0b55c453c4
Melodieshusband <totorkulovredzeb123@gmail.com>
2026-04-05 14:38:33 +0300

    Create Test_bytecode.lua

---
+++ deobfuscated/Test_bytecode.lua
@@ -0,0 +1,32 @@
+      Cilent Right Korblox	   DarkTheme   game   KeyCode   V7   This file was protected with MoonSec V3 by Federal#9999   NewTab
+   NewKeybind	   CreateLib   Gives Fake Korblox
+   _TIfvHPaIwCyW   Main
+   Toggle GUI   Enum
+   NewSection   Gives Fake Headless   Keybind   Main Stuffs   Changes Keybind   HttpGet   Cilent Left Korblox
+   loadstring	   NewButton   Headless And Korblox   Cilent HeadlessH   https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua   MoonSec_StringsHiddenAttr6                      &      7      ,      !      $                             	          	 (                                              .              #      +         
+       
+   #      +         	       
+   #      %           
+                        .    
+  
+    	    ! 
+      
+ 
+    
+ 
+  -       4                             ToggleUI            	             ,              	   Character)   http://www.roblox.com/asset/?id=902942096   Transparency
+   RightLowerLeg)   http://www.roblox.com/asset/?id=902942089%   http://roblox.com/asset/?id=902843398   MeshId
+   RightUpperLeg	   TextureID	   RightFoot)   http://www.roblox.com/asset/?id=902942093   LocalPlayer
+   GetService   Players      р?   game               
+                               
+ 0     *    
+ 0     
+     0          0          0          0  	   ,              
+   GetService	   TextureID   game   LeftLowerLeg   MeshId   Transparency   LocalPlayer	   Character)   http://www.roblox.com/asset/?id=902942101   LeftFoot      р?%   http://roblox.com/asset/?id=902842271)   http://www.roblox.com/asset/?id=902942077   LeftUpperLeg   Players)   http://www.roblox.com/asset/?id=902942082                                              
+ 0   
+  *    
+ 0          0   	       0
+          0          0     ,                 Head   Transparency   game   LocalPlayer   pairs   GetChildren   Decal	   Character      р?   IsA   Players                                      0    	                                   0    	 
+                                                 
+      	   
+ (               7     7     0   	 :      7      ,           
